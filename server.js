db6e3c47abd1f4a00a01930498c7fdfbfb19c731
Tumu Rohit Kumar <118418663+rohit-0604@users.noreply.github.com>
2025-07-11 09:57:19 +0530

    Update server.js

--- server.js
+++ server.js
@@ -14,39 +14,14 @@ const port = process.env.PORT || 4000
 connectDB()
 connectCloudinary()
 
-// ✅ CORS Setup connect frontend to backend
-const allowedOrigins = [
-  'https://prescriptodoctorappointmentapp.netlify.app',
-  'https://prescriptoadmindoctorpanel.netlify.app'
-];
-
-app.use(cors({
-  origin: function (origin, callback) {
-    // Allow requests with no origin (like mobile apps, curl, Postman)
-    if (!origin || allowedOrigins.includes(origin)) {
-      callback(null, true);
-    } else {
-      callback(new Error('Not allowed by CORS'));
-    }
-  },
-  credentials: true
-}));
-
-// app.options('*', cors()); // Handle preflight requests for all routes
-
 // Middleware
 app.use(express.json()) // requests gets passes through this
+app.use(cors())  // connect frontend to backend
 
 // Api endpoints
-console.log("Mounting /api/admin routes...");
-app.use('/api/admin', adminRouter);
-
-console.log("Mounting /api/doctor routes...");
-app.use('/api/doctor', doctorRouter);
-
-console.log("Mounting /api/user routes...");
-app.use('/api/user', userRouter);
-
+app.use('/api/admin',adminRouter)
+app.use('/api/doctor',doctorRouter)
+app.use('/api/user',userRouter)
 // localhost:4000/api/admin
 
 app.get('/',(req,res)=>{
